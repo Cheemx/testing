fc50a1be890ff8c65c5457e2781eaa9f8ac940e2
Cheemx <chinmaymahajan999@gmail.com>
2025-08-22 18:32:20 +0530

    Tested & lovely version of Hello, World!

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Cheems")
-	want := "Hello, Cheems"
+	// Subtest to test regular functioning of Hello
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Cheems")
+		want := "Hello, Cheems"
 
+		assertCorrectMessage(t, got, want)
+	})
+	// Subtest to test empty strings in Hello
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// Refactored the assertion into a helper function!
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this is a helper method. By doing this, when it fails, the line number reported will be in our function call rather than inside our test helper. This will help other developers track down problems more easily.
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
