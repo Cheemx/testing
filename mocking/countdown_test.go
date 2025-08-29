1bd2dd7fb58229a5107aca49bdebff92becf5c34
Cheemx <chinmaymahajan999@gmail.com>
2025-08-29 20:23:25 +0530

    mocking commit 01

---
+++ mocking/countdown_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// We want our Countdown function to write data somewhereand io.Writer is the de-facto (illegal but still accepted) way of capturing that as an interface in Go.
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
