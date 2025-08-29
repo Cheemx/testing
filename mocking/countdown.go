0e69433875cd12cbbb2c21141014866a74d7043c
Cheemx <chinmaymahajan999@gmail.com>
2025-08-29 22:02:32 +0530

    mocking brainf*ck

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -17,6 +17,36 @@ type SpySleeper struct {
 
 type DefaultSleeper struct{}
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
@@ -27,6 +57,8 @@ func (s *SpySleeper) Sleep() {
 
 const finalWord = "Go!"
 const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 func Countdown(w io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
@@ -37,6 +69,6 @@ func Countdown(w io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
