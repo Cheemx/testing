379e1535e3f8bc587c2e9f1173900596ae3bb902
Cheemx <chinmaymahajan999@gmail.com>
2025-09-10 17:13:21 +0530

    httptest application init

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/Cheemx/testing/server
+
+go 1.25.0
---
+++ server/server/server_test.go
@@ -0,0 +1,38 @@
+package server_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Cheemx/testing/server/server"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		server.PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
